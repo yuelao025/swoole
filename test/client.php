4eeadc3f0e2c49d5c90e018115755e056bbf7b69
wanmin <wanminny@163.com>
2017-07-06 14:50:30 +0800

    add tcp cli and nc -l -p port

---
+++ test/client.php
@@ -0,0 +1,29 @@
+<?php
+
+//nc -l -p  9501
+
+class Client
+{
+    private $client;
+    public function __construct() {
+        $this->client = new swoole_client(SWOOLE_SOCK_TCP);
+    }
+    public function connect() {
+        if( !$this->client->connect("127.0.0.1", 9501 , 1) ) {
+            echo "Error: {$this->client->errCode}\n";
+            exit;
+        }
+
+        $msg_normal = "This is a Msg test!\r\n";
+        // $msg_eof = "This is a Msg\r\n";
+        $msg_length = pack("N" , strlen($msg_normal) ). $msg_normal;
+        $i = 0;
+        while( $i < 10 ) {
+            $this->client->send( $msg_length );
+            $i ++;
+        }
+    }
+}
+$client = new Client();
+$client->connect();
+
